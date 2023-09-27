69e537e980f496ec813b8306c5f29fe548090870
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2023-09-27 08:28:42 -0600

    Math and more

---
+++ Math.java
@@ -0,0 +1,15 @@
+
+
+public class Math {
+    public static void main(String[] args)
+    {
+        System.out.println(java.lang.Math.abs(-4.7)); 
+        System.out.println(java.lang.Math.max(1, 87)); 
+        System.out.println(java.lang.Math.min(1, 87)); 
+        System.out.println(java.lang.Math.sqrt(64));
+        System.out.println(java.lang.Math.random());
+
+        int random = (int)(java.lang.Math.random() * 1001);
+        System.out.println(random);
+    }
+}
